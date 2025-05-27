ccb8b394526c32d63656c71f30ddb3d7446d7a3d
DnyaneshU <112575291+DnyaneshU@users.noreply.github.com>
2025-05-27 10:02:45 +0530

    Test_sample.c

--- tests/test_sample.c
+++ tests/test_sample.c
@@ -1,8 +1,13 @@
 #include <assert.h>
 #include "../fixes/sample_fixed.c"
 
+void test_multiply() {
+    assert(multiply(2, 3) == 6);
+    assert(multiply(5, 0) == 0);
+    assert(multiply(-1, 4) == -4);
+}
+
 int main() {
-    assert(add(2, 3) == 5);
-    assert(add(0, 0) == 0);
+    test_multiply();
     return 0;
 }
